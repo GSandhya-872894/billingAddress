e78f3dbc68ae42ebbeea5264419a8396181dffe6
GSandhya-872894 <72181310+GSandhya-872894@users.noreply.github.com>
2020-10-22 09:39:00 +0530

    Add files via upload

---
+++ billaddress/billaddress.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-billaddress',
+  templateUrl: './billaddress.component.html',
+  styleUrls: ['./billaddress.component.css']
+})
+export class BilladdressComponent implements OnInit {
+  billForm: FormGroup;
+  constructor( private formBuilder : FormBuilder ) { }
+  
+  ngOnInit(): void {
+    this.billForm = this.formBuilder.group({
+      firstName : ['', Validators.required],
+      lastName : ['', Validators.required],
+      buildingNo : ['', Validators.required],
+      streetName : ['', Validators.required],
+      city : ['', Validators.required],
+      state : ['', Validators.required],
+      zipCode : ['', Validators.required],
+      landmark : ['', Validators.required],
+      phoneNo : ['', Validators.required]
+    })
+  }
+
+  get f() {
+    return this.billForm.controls;
+  }
+
+
+}
